613be747b00476dd8ed7d89e90f0808a3271d73f
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2022-05-21 17:58:52 +0100

    Add files via upload

---
+++ src/components/Header/Nav.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+import TranslatedText from '../TranslatedText'
+
+const Nav: React.FC = () => {
+  return (
+    <StyledNav>
+      <StyledAbsoluteLink href="https://tokenpad.finance">
+        <TranslatedText translationId={2}>Home</TranslatedText>
+      </StyledAbsoluteLink>
+      <StyledAbsoluteLink href="#">
+        <TranslatedText translationId={2}>Farming</TranslatedText>
+      </StyledAbsoluteLink>
+      <StyledAbsoluteLink href="#">
+        <TranslatedText translationId={4}>Staking</TranslatedText>
+      </StyledAbsoluteLink>
+      <StyledAbsoluteLink href="#">
+        Chart
+      </StyledAbsoluteLink>
+      <StyledAbsoluteLink href="#">
+        Launchpad
+      </StyledAbsoluteLink>
+      <StyledAbsoluteLink href="#">
+        Whitepaper
+      </StyledAbsoluteLink>
+    </StyledNav>
+  )
+}
+
+const StyledNav = styled.nav`
+  align-items: center;
+  display: flex;
+  font-size: 20px;
+  line-height: 45px;
+  font-weight: 500;
+  @media (max-width: 600px) {
+    display: none;
+  }
+`
+
+const StyledAbsoluteLink = styled.a`
+  color: #F9B234;
+  padding-left: 10px;
+  padding-right: 10px;
+  text-decoration: none;
+  &:hover {
+    color: #452a7a;
+  }
+  &.active {
+    color: #452a7a;
+  }
+  @media (max-width: 400px) {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+`
+
+export default Nav
