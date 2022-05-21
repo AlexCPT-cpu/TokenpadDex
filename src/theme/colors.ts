dc850bf5cc5fc09cb831042307f436e9bcc9e518
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2022-05-21 17:53:49 +0100

    Add files via upload

---
+++ src/theme/colors.ts
@@ -0,0 +1,22 @@
+export const white = '#FFFFFF'
+export const black = '#000000'
+
+export const green = {
+  green1: '#6560db',
+  green2: '#6963cf'
+}
+
+export const red = {
+  red1: '#FF6871',
+  red2: '#F82D3A'
+}
+
+export const yellow = {
+  yellow1: '#FFE270',
+  yellow2: '#F3841E'
+}
+
+export const blue = {
+  yellow1: '#FFE270',
+  yellow2: '#F3841E'
+}
